598de0645349ab817ea4da350f419a0918c6efd4
Bradley Walters <zerfgog202@gmail.com>
2017-02-26 19:23:13 -0700

    Fix lint worker erroring if gluaLintString was not available after a single frame

--- lint_worker.js
+++ lint_worker.js
@@ -1,10 +1,21 @@
 importScripts("bower_components/glualint-lib/compiled.js")
 
 onmessage = (e) => {
-  setTimeout(() => {
+  if (typeof gluaLintString !== "undefined") {
     postMessage({
       id: e.data.id,
       result: gluaLintString(e.data.code)
     })
-  }, 0)
+  } else {
+    let interval = setInterval(() => {
+      if (typeof gluaLintString !== "undefined") {
+        clearInterval(interval)
+
+        postMessage({
+          id: e.data.id,
+          result: gluaLintString(e.data.code)
+        })
+      }
+    }, 0)
+  }
 }
